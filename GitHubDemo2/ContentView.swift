483a75a9ccb187909a99fc7fa238401fb42fa33b
nurbekataboyev <nurbek.ataboyev.prog@gmail.com>
2023-11-10 23:40:16 +0500

    Initial Commit

---
+++ GitHubDemo2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubDemo2
+//
+//  Created by Nurbek on 10/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to SwiftUI")
+        }
+        .padding()
+    }
+}
+
+//#Preview {
+//    ContentView()
+//}
---
+++ GitHubDemo2/GitHubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubDemo2App.swift
+//  GitHubDemo2
+//
+//  Created by Nurbek on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
